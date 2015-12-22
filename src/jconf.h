b36e5b5b33ce32a4f78affdfe026b59d1d703710
Young Lee <ncisoft@hotmail.com>
2015-12-23 04:31:35 +0800

    update MAX_CONNECT_TIMEOUT from 10 to 120, in order to prevent error message as 'TCP connection timeout' while connect to slow remote host

--- src/jconf.h
+++ src/jconf.h
@@ -23,10 +23,10 @@
 #define _JCONF_H
 
 #define MAX_PORT_NUM 1024
-#define MAX_REMOTE_NUM 10
+#define MAX_REMOTE_NUM 100
 #define MAX_CONF_SIZE 128 * 1024
 #define MAX_DNS_NUM 4
-#define MAX_CONNECT_TIMEOUT 10
+#define MAX_CONNECT_TIMEOUT 120
 #define MIN_UDP_TIMEOUT 10
 
 typedef struct {
