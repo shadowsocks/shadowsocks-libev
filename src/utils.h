c6123fa32f2a5ab98078b13abc53dee052ea9c2f
Adam Anonymous <anonymous_temp_mail@yahoo.co.jp>
2017-02-01 10:32:35 +0800

    ss-libev: local: Use enum number for val of long options
    Signed-off-by: Adam Anonymous <anonymous_temp_mail@yahoo.co.jp>

--- src/utils.h
+++ src/utils.h
@@ -161,6 +161,15 @@ extern int use_syslog;
 
 #endif // if ANDROID
 
+/* Vals for long options */
+enum { GETOPT_VAL_HELP = 257,
+       GETOPT_VAL_FAST_OPEN,
+       GETOPT_VAL_ACL,
+       GETOPT_VAL_MTU,
+       GETOPT_VAL_MPTCP,
+       GETOPT_VAL_PLUGIN,
+       GETOPT_VAL_PLUGIN_OPTS };
+
 void ERROR(const char *s);
 
 char *ss_itoa(int i);
